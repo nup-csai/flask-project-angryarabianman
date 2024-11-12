83a88cfc36fb7866871e774d108f2a85fca5e338
angryarabianman <londonboy.ilya@gmail.com>
2024-11-12 14:27:00 +0200

    test

--- app/server.py
+++ app/server.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
 
-
+Tasks = [{
+"name": "Task 1",
+"starting_date": "12.11.2024",
+"ending_date": "13.11.2024",
+"is_complete": False,
+"description": "qweqwe"},
+{
+"name": "Task 2",
+"starting_date": "01.12.2024",
+"ending_date": "02.11.2024",
+"is_complete": True,
+"description": "asdasd"}
+]
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    return str(Tasks)
