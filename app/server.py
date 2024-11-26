85cb4c0685348d220bb1512ff17466c3d09fd055
angryarabianman <londonboy.ilya@gmail.com>
2024-11-26 14:57:21 +0200

    changed

--- app/server.py
+++ app/server.py
@@ -1,25 +1,25 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
-Tasks = [{
-    "name": "Task 1",
-    "starting_date": "12.11.2024",
-    "ending_date": "13.11.2024",
-    "is_complete": False,
-    "description": "qweqwe"
-},
-{
-    "name": "Task 2",
-    "starting_date": "01.12.2024",
-    "ending_date": "02.11.2024",
-    "is_complete": True,
-    "description": "asdasd"
-}]
-
+Tasks = []
 app = Flask(__name__)
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
     return render_template('index.html', tasks=Tasks)
+
+@app.route('/add_task', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        Tasks.append({
+            'name': request.form['task_name'],
+            'starting_date': request.form['starting_date'],
+            'ending_date': request.form['ending_date'],
+            'is_complete': False,
+            'description': request.form['description']
+        })
+        return redirect(url_for('add_task'))
+
+    return render_template('add_task_form.html')
 
 @app.route('/tasks_json')
 def tasks_json():
---
+++ docs.md
@@ -0,0 +1,11 @@
+/add_task (GET) - shows the html page with the form for adding a task \
+/add_task (POST) - add the task to the list using the information provided in the form
+
+
+Route 'home' (/): \
+Shows: list of all tasks (starting date, ending date, status, description)
+
+Route 'add_task' (/add_task (POST)): \
+Shows: the form where info of a task can be entered \
+Actions: add task to the list
+Elements: "Add" button
